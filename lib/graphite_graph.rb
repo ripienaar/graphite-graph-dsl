7b91479c8b1c9ef4947dc6fe74307034e9ea17b4
Jiri Barton <jbar@hosting4u.cz>
2012-06-04 17:59:41 +0200

    added full support for asPercent

--- lib/graphite_graph.rb
+++ lib/graphite_graph.rb
@@ -297,7 +297,11 @@ class GraphiteGraph
         elsif target[:scale_to_seconds]
           graphite_target = "scaleToSeconds(#{graphite_target},#{target[:scale_to_seconds]})"
         end
-        graphite_target = "asPercent(#{graphite_target})" if target[:as_percent]
+        if target[:as_percent] == true
+          graphite_target = "asPercent(#{graphite_target})"
+        elsif target[:as_percent]
+          graphite_target = "asPercent(#{graphite_target},#{target[:as_percent]})"
+        end
         graphite_target = "drawAsInfinite(#{graphite_target})" if target[:line]
         graphite_target = "movingAverage(#{graphite_target},#{target[:smoothing]})" if target[:smoothing]
 
